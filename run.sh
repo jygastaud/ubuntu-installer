2dec999d2714db04854932b4591e89f8840347c1
Jean Yves GASTAUD <jygastaud@gmail.com>
2016-01-07 00:55:18 +0100

    WIP run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
+# Install ansible
 
+# Download galaxy roles
+ansible-galaxy install -r requirements.yml --force
+
+# Provisionning
 ansible-playbook playbook.yml -i hosts --user=$(whoami) --ask-sudo-pass
